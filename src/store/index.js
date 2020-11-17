345536e2307fb593cadba4dcb72d9e32660a5117
anna <anna@wax.be>
2020-11-17 22:58:11 +0100

    hypixel-api implemented

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,34 @@
 // import { update } from "lodash";
 import Vue from "vue";
 import Vuex from "vuex";
+import hypixel from "hypixel-api";
 
 Vue.use(Vuex);
+const client = new hypixel('18b8d682-a0a4-4c55-a555-ac4420eee87c')
 
 export default new Vuex.Store({
   state: {
-    name: 'fANNAtastisch'
+    name: null,
+    player: null
   },
   mutations: {
     updateName(state, updated_name){
       state.name = updated_name
+    },
+    updatePlayer(state, data) {
+      state.player = data
     }
   },
   actions: {
     updateName(context, updated_name){
       context.commit("updateName", updated_name)
+    },
+    fetchPlayer(context, player_name){
+      client.getPlayer('name', player_name).then((player) => {
+        context.commit('updatePlayer', player)
+      }).catch((err) => {
+        console.error('Error! ' + err)
+      })
     }
   },
   getters: {
